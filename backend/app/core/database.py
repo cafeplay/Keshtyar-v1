1d9ec0f68d261c3e6ac2d4be3cec455e4d2233c0
cafeplay <69569458+cafeplay@users.noreply.github.com>
2026-06-22 02:20:37 +0330

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,10 +1,17 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.engine import make_url
 from app.core.config import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
-# اگه با asyncpg مشکل داشت، اینو امتحان کن
+# دیباگ - لاگ گرفتن از اطلاعات اتصال
+url = make_url(DATABASE_URL)
+print(f"DB USER = {url.username}")
+print(f"DB HOST = {url.host}")
+print(f"DB PORT = {url.port}")
+
+# تبدیل به asyncpg
 if DATABASE_URL.startswith("postgresql://"):
     DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
 
