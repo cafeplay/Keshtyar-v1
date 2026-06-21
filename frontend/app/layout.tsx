316e39f47b9973a4794394e0533178d0e394e159
cafeplay <69569458+cafeplay@users.noreply.github.com>
2026-06-21 20:08:11 +0330

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fa" dir="rtl">
-      <body>
+      <body className="bg-[#F9F8F6] dark:bg-[#121212] text-gray-800 dark:text-gray-200">
         {children}
       </body>
     </html>
